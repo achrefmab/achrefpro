63cb198a353fc99be5c13869d9c125a735883b65
achref <achref.mabrouk12@gmail.com>
2020-06-06 09:58:38 +1400

    okey

---
+++ online-commerce/src/test/java/com/ecommerce/onlinecommerce/OnlineCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.onlinecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
